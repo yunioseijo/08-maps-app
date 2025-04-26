45b3f171bccc8ca0aeb41cbd2018f1554bfdf5b9
yunioseijo <yunioseijo@gmail.com>
2025-04-26 16:41:26 +0200

    feat: add navbar and multiple page components with basic templates

---
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fullscreen-map-page',
+  imports: [],
+  templateUrl: './fullscreen-map-page.component.html',
+})
+export class FullscreenMapPageComponent { }
---
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markers-page',
+  imports: [],
+  templateUrl: './markers-page.component.html',
+})
+export class MarkersPageComponent { }
