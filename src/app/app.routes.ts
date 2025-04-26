dc4cf0626c1fe2ce2c4d6afcecbbca831d0b95a7
yunioseijo <yunioseijo@gmail.com>
2025-04-26 16:50:02 +0200

    feat: update routing and implement navbar component with dropdown menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPageComponent } from './pages/fullscreen-map-page/fullscreen-map-page.component';
+import { HousesPageComponent } from './pages/houses-page/houses-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullscreenMapPageComponent,
+    title: 'Fullscreen Map',
+  },
+  {
+    path: 'houses',
+    component: HousesPageComponent,
+    title: 'Propiedades disponibles',
+  },
+  {
+    path: 'markers',
+    component: MarkersPageComponent,
+    title: 'Marcadores',
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen',
+  }
+];
